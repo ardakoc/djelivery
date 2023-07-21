0e34b2bb7185514d3408aa6c21b9fed37e1961b4
Arda Koç <ardaskoc@gmail.com>
2023-07-21 17:17:44 +0300

    added restaurant icon.

---
+++ frontend/src/components/Icons/Restaurant.jsx
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStore } from '@fortawesome/free-solid-svg-icons'
+
+export default function Restaurant() {
+  return <FontAwesomeIcon icon={faStore} style={{color: "#12b886", width: "16px",}} />
+}
