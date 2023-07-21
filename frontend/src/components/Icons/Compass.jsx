948ce6e9639cf59f635f130c11440dcfa82e810d
Arda Koç <ardaskoc@gmail.com>
2023-07-21 17:16:19 +0300

    updated icon style.

--- frontend/src/components/Icons/Compass.jsx
+++ frontend/src/components/Icons/Compass.jsx
@@ -2,5 +2,5 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCompass } from '@fortawesome/free-solid-svg-icons'
 
 export default function Compass() {
-  return <FontAwesomeIcon icon={faCompass} style={{color: "#12b886", paddingRight: "8px"}} />
+  return <FontAwesomeIcon icon={faCompass} style={{color: "#12b886",}} />
 }
--- frontend/src/components/Icons/Location/Location.jsx
+++ frontend/src/components/Icons/Location/Location.jsx
@@ -2,5 +2,5 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 export default function Location() {
-  return <FontAwesomeIcon icon={faLocationDot} style={{color: "#12b886", paddingRight: "8px"}} />
+  return <FontAwesomeIcon icon={faLocationDot} style={{color: "#12b886", width: "16px",}} />
 }
