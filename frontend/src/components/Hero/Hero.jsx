84911ddc7cbea3589e43ac52be90db2cc7889270
Arda Koç <ardaskoc@gmail.com>
2023-07-22 12:09:07 +0300

    removed hero banner component. added background prop for hero component instead.

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,14 +1,11 @@
 // Bootstrap Components
 import Row from 'react-bootstrap/Row';
-import Image from 'react-bootstrap/Image';
-import HeroBanner from '../../assets/img/hero-banner.jpg'
 import HeroForm from './HeroForm';
 
 export default function Hero() {
   return (
     <>
-      <Row>
-        <Image src={HeroBanner} fluid className="banner"/>
+      <Row className="hero">
         <HeroForm />
       </Row>
     </>
