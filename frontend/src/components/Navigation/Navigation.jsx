dbfce45a8b85f9d26269c544615bd7e87897028f
Arda Koç <ardaskoc@gmail.com>
2023-08-03 16:55:58 +0300

    the modal seperated from navigation component.

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,82 +1,109 @@
+// React Hooks
+import { useState } from "react";
+
 // Bootstrap Components
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
 
 // Custom Components
-import FoodMenu from './FoodMenu/FoodMenu';
+import FoodMenu from "./FoodMenu/FoodMenu";
 
 // Logo
-import NavLogo from './NavLogo';
+import NavLogo from "./NavLogo";
 
 // Icons
-import Compass from '../Icons/Compass';
-import Location from '../Icons/Location/Location';
+import Compass from "../Icons/Compass";
+import Location from "../Icons/Location/Location";
+import LoginRegisterModal from "../Modals/LoginRegisterModal";
 
 export default function Navigation() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
+
   return (
-    <Navbar expand="lg" className="navbar" sticky="top" >
-      <Container>
-        <NavLogo />
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-2"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1" className="fw-bold">Restaurants</Nav.Link>
-            <NavDropdown 
-              title={
-                <div style={{ display: "inline-block", paddingRight: "8px", }}>
-                  <Compass />
-                  <span style={{ paddingLeft: "8px" }}>WHAT TO EAT / DRINK?</span>
-                </div>
+    <>
+      <Navbar expand="lg" className="navbar" sticky="top">
+        <Container>
+          <NavLogo />
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-2"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1" className="fw-bold">
+                Restaurants
+              </Nav.Link>
+              <NavDropdown
+                title={
+                  <div style={{ display: "inline-block", paddingRight: "8px" }}>
+                    <Compass />
+                    <span style={{ paddingLeft: "8px" }}>
+                      WHAT TO EAT / DRINK?
+                    </span>
+                  </div>
                 }
                 id="navbarScrollingDropdown"
                 style={{ fontSize: "14px", paddingTop: "2px" }}
-            >
-              <FoodMenu />
-            </NavDropdown>
-            <NavDropdown 
-              title={
-                <div style={{ display: "inline-block", paddingRight: "8px" }}>
-                  <Location />
-                  <span style={{ paddingLeft: "8px" }}>CHOOSE LOCATION</span>
-                </div>
+              >
+                <FoodMenu />
+              </NavDropdown>
+              <NavDropdown
+                title={
+                  <div style={{ display: "inline-block", paddingRight: "8px" }}>
+                    <Location />
+                    <span style={{ paddingLeft: "8px" }}>CHOOSE LOCATION</span>
+                  </div>
                 }
                 id="navbarScrollingDropdown"
                 style={{ fontSize: "14px", paddingTop: "2px" }}
-            >
-              <Form.Control 
-                type="search"
-                placeholder="Search locations"
-                aria-label="Search Locations"
-                className="ps-2 me-2 ms-2"
-                style={{
-                  width: "auto",
-                  height: "32px",
-                  fontSize: "12px"
-                }}
-              />
-              <NavDropdown.Divider />
-              <NavDropdown.Header>Popular Locations</NavDropdown.Header>
-              <NavDropdown.Item href="#action4">İstanbul</NavDropdown.Item>
-              <NavDropdown.Item href="#action5">Ankara</NavDropdown.Item>
-              <NavDropdown.Item href="#action6">İzmir</NavDropdown.Item>
-              <NavDropdown.Item href="#action7">Antalya</NavDropdown.Item>
-              <NavDropdown.Item href="#action8">Bursa</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav className='d-flex gap-3'>
-            <Nav.Link href="#action1" className="fw-bold" style={{ fontSize: "14px", letterSpacing: "-0.3px" }}>LOGIN / REGISTER</Nav.Link>
-            <Button className="btn-success fw-bold" style={{ fontSize: "14px", letterSpacing: "-0.3px" }}>REGISTER RESTAURANT</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
+              >
+                <Form.Control
+                  type="search"
+                  placeholder="Search locations"
+                  aria-label="Search Locations"
+                  className="ps-2 me-2 ms-2"
+                  style={{
+                    width: "auto",
+                    height: "32px",
+                    fontSize: "12px",
+                  }}
+                />
+                <NavDropdown.Divider />
+                <NavDropdown.Header>Popular Locations</NavDropdown.Header>
+                <NavDropdown.Item href="#action4">İstanbul</NavDropdown.Item>
+                <NavDropdown.Item href="#action5">Ankara</NavDropdown.Item>
+                <NavDropdown.Item href="#action6">İzmir</NavDropdown.Item>
+                <NavDropdown.Item href="#action7">Antalya</NavDropdown.Item>
+                <NavDropdown.Item href="#action8">Bursa</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav className="d-flex gap-3">
+              <Nav.Link
+                className="fw-bold"
+                style={{ fontSize: "14px", letterSpacing: "-0.3px" }}
+                onClick={handleShowModal}
+              >
+                LOGIN / REGISTER
+              </Nav.Link>
+              <Button
+                className="btn-success fw-bold"
+                style={{ fontSize: "14px", letterSpacing: "-0.3px" }}
+              >
+                REGISTER RESTAURANT
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <LoginRegisterModal show={showModal} onHide={handleCloseModal} />
+    </>
+  );
 }
