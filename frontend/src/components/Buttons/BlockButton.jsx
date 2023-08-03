32e94e16785737c81ccf70cd96c6d37191324856
Arda Koç <ardaskoc@gmail.com>
2023-08-03 17:17:44 +0300

    added custom button components.

---
+++ frontend/src/components/Buttons/BlockButton.jsx
@@ -0,0 +1,17 @@
+// Bootstrap Components
+import Button from "react-bootstrap/Button";
+
+export default function BlockButton(props) {
+  return (
+    <div className="d-grid gap-2">
+      <Button
+        className={props.className || "btn-success fw-bold"}
+        style={props.style || { letterSpacing: "-0.3px" }}
+        variant={props.variant || "success"}
+        type={props.type || "submit"}
+      >
+        {props.text}
+      </Button>
+    </div>
+  );
+}
---
+++ frontend/src/components/Buttons/PrimaryButton.jsx
@@ -0,0 +1,13 @@
+// Bootstrap Components
+import Button from "react-bootstrap/Button";
+
+export default function PrimaryButton(props) {
+  return (
+    <Button
+      className={props.className || "btn-success fw-bold"}
+      style={props.style || { fontSize: "14px", letterSpacing: "-0.3px" }}
+    >
+      {props.text}
+    </Button>
+  );
+}
