1f0e30b2992d78ade4d59a1447b5fc19ce1fb7c8
Arda Koç <ardaskoc@gmail.com>
2023-08-03 17:18:21 +0300

    updated login form.

--- frontend/src/components/Forms/LoginForm.jsx
+++ frontend/src/components/Forms/LoginForm.jsx
@@ -1,5 +1,17 @@
+// Bootstrap Components
+import Form from "react-bootstrap/Form";
+import BlockButton from "../Buttons/BlockButton";
+
 export default function LoginForm() {
   return (
-    <p>Login Form</p>
-  )
+    <Form>
+      <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Control type="email" placeholder="Email address" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formPassword">
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <BlockButton text="LOG IN" />
+    </Form>
+  );
 }
