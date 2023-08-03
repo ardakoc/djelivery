1a28aede7ed2eba3b83f25aee7b44521092ed90e
Arda Koç <ardaskoc@gmail.com>
2023-08-03 17:35:49 +0300

    added new icons.

---
+++ frontend/src/components/Icons/Email.jsx
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAt } from '@fortawesome/free-solid-svg-icons'
+
+export default function Email() {
+  return <FontAwesomeIcon icon={faAt} style={{color: "#12b886",}} />
+}
---
+++ frontend/src/components/Icons/Password.jsx
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faKey } from '@fortawesome/free-solid-svg-icons'
+
+export default function Password() {
+  return <FontAwesomeIcon icon={faKey} style={{color: "#12b886",}} />
+}
