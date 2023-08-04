dec7481b882cfca75c00937f181f255e03ed704f
Arda Koç <ardaskoc@gmail.com>
2023-08-04 13:20:25 +0300

    updated login form component.

--- frontend/src/components/Forms/LoginForm.jsx
+++ frontend/src/components/Forms/LoginForm.jsx
@@ -1,5 +1,9 @@
+// React Hooks
+import { useState } from "react";
+
 // Bootstrap Components
 import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
 
 // Custom Components
 import BlockButton from "../Buttons/BlockButton";
@@ -11,14 +15,35 @@ import Password from "../Icons/Password";
 
 export default function LoginForm() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formEmail">
-        <InputWithIcon icon={<Email />} placeholder="Email address" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formPassword">
-        <InputWithIcon icon={<Password />} placeholder="*********" />
-      </Form.Group>
-      <BlockButton text="LOG IN" />
-    </Form>
+    <>
+      <Form>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <InputWithIcon icon={<Email />} placeholder="Email address" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPassword">
+          <InputWithIcon
+            icon={<Password />}
+            placeholder="*********"
+            type="password"
+          />
+        </Form.Group>
+        <Form.Text>
+          <Nav>
+            <Nav.Link style={{ color: "#000", padding: "0 0 16px 0" }}>
+              Forgot your password?
+            </Nav.Link>
+          </Nav>
+        </Form.Text>
+        <BlockButton text="LOG IN" />
+        <Form.Text>
+          <Nav style={{ paddingTop: "16px" }}>
+            Don't have an account?
+            <Nav.Link style={{ color: "#000", padding: "0 0 0 4px" }}>
+              Register here!
+            </Nav.Link>
+          </Nav>
+        </Form.Text>
+      </Form>
+    </>
   );
 }
