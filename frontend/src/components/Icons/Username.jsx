1bef0d7c91cce172939aea45063691dd5e19aebc
Arda Koç <ardaskoc@gmail.com>
2023-08-06 11:43:54 +0300

    updated username icon.

--- frontend/src/components/Icons/Username.jsx
+++ frontend/src/components/Icons/Username.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUserLarge } from "@fortawesome/free-solid-svg-icons";
 
 export default function Username() {
-  return <FontAwesomeIcon icon={faUser} style={{ color: "#12b886" }} />
+  return <FontAwesomeIcon icon={faUserLarge} style={{ color: "#12b886" }} />
 }
