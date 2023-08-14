ff384079c0ff1aebba78ccf3f9aee64b31df2ac8
Arda Koç <ardaskoc@gmail.com>
2023-08-14 14:01:09 +0300

    updated vendor serializer.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from accounts.models import User
+from accounts.models import User, UserProfile
 from vendor.models import Vendor
 
 
@@ -22,8 +22,29 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class VendorSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Vendor
-        fields = [
-            'vendor_name', 'vendor_license',
-        ]
+        fields = ['user', 'vendor_name', 'vendor_license']
+
+    def create(self, validated_data):
+        user_data = validated_data['user']
+        user = User.objects.create_user(
+            user_data['first_name'],
+            user_data['last_name'],
+            user_data['username'],
+            user_data['email'],
+            user_data['password']
+        )
+        user.role = 1
+        user.save()
+        user_profile = UserProfile.objects.get(user=user)
+        validated_data.pop('user')
+        vendor = Vendor.objects.create(
+            user=user,
+            user_profile=user_profile,
+            **validated_data
+        )
+
+        return vendor
