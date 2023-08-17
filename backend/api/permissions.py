597ef4e7af8e59bcb0d96706a2b2fdddcb4558d8
Arda Koç <ardaskoc@gmail.com>
2023-08-17 16:38:17 +0300

    added custom permission class.

---
+++ backend/api/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+class IsAnonymousUser(permissions.BasePermission):
+    """
+    Permission check for anonymous user.
+    """
+
+    def has_permission(self, request, view):
+        if request.user and request.user.is_authenticated:
+            return False
+        return True
