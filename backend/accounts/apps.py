43ab1b6e94aa23dbf42ff321b9f8dfccb6c54329
Arda Koç <ardaskoc@gmail.com>
2023-08-21 14:57:42 +0300

    fixed signal error.

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-from django.core.signals import request_finished
 
 
 class AccountsConfig(AppConfig):
@@ -8,5 +7,3 @@ class AccountsConfig(AppConfig):
 
     def ready(self):
         from . import signals
-
-        request_finished.connect(signals.post_save_create_profile_receiver)
--- backend/accounts/signals.py
+++ backend/accounts/signals.py
@@ -6,11 +6,6 @@ from .models import User, UserProfile
 
 
 @receiver(post_save, sender=User)
-def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-    else:
-        try:
-            UserProfile.objects.get(user=instance).save()
-        except ObjectDoesNotExist:
-            UserProfile.objects.create(user=instance)
