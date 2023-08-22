c4ad07d16a7bd7c199e4e68cb4aa385255295398
Arda Koç <ardaskoc@gmail.com>
2023-08-22 16:08:13 +0300

    added test class for user api. added some test methods.

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+User = get_user_model()
+
+class UserAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            first_name='test',
+            last_name='user',
+            email='testuser@test.com',
+            username='testuser',
+            password='testpassword',
+        )
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+
+    def test_retrieve_authenticated_user(self):
+        response = self.client.get('/api/v1/users/{}/'.format(self.user.uuid))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], self.user.username)
+
+    def test_retrieve_unauthenticated_user(self):
+        self.client.credentials()  # Remove credentials
+        response = self.client.get('/api/v1/users/{}/'.format(self.user.uuid))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
