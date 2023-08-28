54b7c57b3442023d1dafc4a81c543e3525e6984d
Arda Koç <ardaskoc@gmail.com>
2023-08-28 18:01:45 +0300

    connected forgot password page with backend.

---
+++ frontend/src/components/Forms/ForgotPasswordForm.jsx
@@ -0,0 +1,77 @@
+// React Hooks
+import axios from "axios";
+import { useState } from "react";
+
+// Bootstrap Components
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/esm/Col";
+
+// Custom Components
+import BlockButton from "../Buttons/BlockButton";
+import InputWithIcon from "../InputGroups/InputWithIcon";
+
+// Icons
+import Email from "../Icons/Email";
+import FormErrorText from "../Texts/FormErrorText";
+
+export default function ForgotPasswordForm(props) {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [emailErrorMsg, setEmailErrorMsg] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      email: event.target.email.value,
+    };
+
+    await axios
+      .post("http://localhost:8000/api/v1/forgot-password/", data)
+      .then((response) => {
+        setIsSubmitted(true);
+      })
+      .catch((error) => {
+        console.log(error)
+        console.log(error.response)
+        console.log(error.response.data)
+        if (error.response.data["error"]) {
+          setEmailErrorMsg(
+            String(error.response.data["error"]).charAt(0).toUpperCase() +
+              String(error.response.data["error"]).slice(1)
+          );
+        } else {
+          setEmailErrorMsg("");
+        }
+      });
+  };
+  return (
+    <>
+      {isSubmitted ? (
+        <p>
+          Password reset link is sent to your email. Please check it and reset
+          your password by click the link we sent.
+        </p>
+      ) : (
+        <>
+          Enter your email address below and we'll send you an email with
+          instructions on how to change your password.
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3 mt-3" controlId="formLastName">
+              <InputWithIcon
+                name="email"
+                type="email"
+                placeholder="Enter your email address"
+                className=""
+                icon={<Email />}
+              />
+            </Form.Group>
+            <FormErrorText msg={emailErrorMsg} />
+            <BlockButton
+              text="SUBMIT"
+              className="btn-success fw-bold mb-4"
+            />
+          </Form>
+        </>
+      )}
+    </>
+  );
+}
--- frontend/src/components/Modals/ForgotPasswordModal.jsx
+++ frontend/src/components/Modals/ForgotPasswordModal.jsx
@@ -3,11 +3,9 @@ import Modal from "react-bootstrap/Modal";
 import Nav from "react-bootstrap/Nav";
 
 // Custom Components
-import BlockButton from "../Buttons/BlockButton";
-import InputWithIcon from "../InputGroups/InputWithIcon";
+import ForgotPasswordForm from "../Forms/ForgotPasswordForm";
 
 // Icons
-import Email from "../Icons/Email";
 import ModalLink from "../Link/ModalLink";
 
 export default function ForgotPasswordModal(props) {
@@ -16,16 +14,11 @@ export default function ForgotPasswordModal(props) {
       <Modal.Header closeButton>
         <Modal.Title>Reset Your Password</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        Enter your email address below and we'll send you an email with
-        instructions on how to change your password.
-        <InputWithIcon
-          icon={<Email />}
-          placeholder="Enter your email address"
-          type="email"
-          className="mb-2 mt-2"
-        />
-        <BlockButton text="SUBMIT" className="btn-success fw-bold mt-2 mb-4" />
+      <Modal.Body>        
+        <ForgotPasswordForm
+          onClickForgotPassword={props.onClickForgotPassword}
+          onClickRegister={props.onClickRegister}
+        />        
         <Nav>
           <ModalLink
             text="Back to login page here"
