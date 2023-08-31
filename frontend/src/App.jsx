5af75f060c47ca1f01e2cb5cc72e5c7287315e41
Arda Koç <ardaskoc@gmail.com>
2023-08-31 11:55:24 +0300

    updated footer style for sticky positioning.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,20 +14,28 @@ import Homepage from "./components/Pages/Homepage/Homepage";
 import Footer from "./components/Pages/Footer/Footer";
 import RestaurantRegister from "./components/Pages/RestaurantRegister/RestaurantRegister";
 import Dashboard from "./components/Pages/Dashboard/Dashboard";
+import ResetPassword from "./components/Pages/ResetPassword/ResetPassword";
 
 function App() {
-
   return (
     <>
       <Router>
+        <div className="wrapper">
         <Navigation />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/register-restaurant" component={RestaurantRegister} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Redirect to="/" />
-        </Switch>
-        <Footer />
+          <Switch>
+            <div className="content">
+              <Route exact path="/" component={Homepage} />
+              <Route
+                path="/register-restaurant"
+                component={RestaurantRegister}
+              />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/reset-password" component={ResetPassword} />
+            </div>
+            <Redirect to="/" />
+          </Switch>
+          <Footer />
+        </div>
       </Router>
     </>
   );
